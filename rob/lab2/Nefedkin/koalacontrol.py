f79fa7f2432b4936c1d89777a6db36d9e38434cb
VitalyaNef <vitlyanef@mail.ru>
2020-05-07 14:33:08 +0300

    Лабораторная работа 2

---
+++ rob/lab2/Nefedkin/koalacontrol.py
@@ -0,0 +1,42 @@
+"""koalacontrol."""
+
+# You may need to import some classes of the controller module. Ex:
+#  from controller import Robot, Motor, DistanceSensor
+from controller import Robot
+
+# create the Robot instance.
+robot = Robot()
+
+# get the time step of the current world.
+timestep = int(robot.getBasicTimeStep())
+
+leftMotor = robot.getMotor('left wheel motor')
+rightMotor = robot.getMotor('right wheel motor')
+
+leftMotor.setPosition(float('inf'))
+rightMotor.setPosition(float('inf'))
+
+leftMotor.setVelocity(0.9)
+rightMotor.setVelocity(0.6)
+
+
+# You should insert a getDevice-like function in order to get the
+# instance of a device of the robot. Something like:
+#  motor = robot.getMotor('motorname')
+#  ds = robot.getDistanceSensor('dsname')
+#  ds.enable(timestep)
+
+# Main loop:
+# - perform simulation steps until Webots is stopping the controller
+while robot.step(timestep) != -1:
+    # Read the sensors:
+    # Enter here functions to read sensor data, like:
+    #  val = ds.getValue()
+
+    # Process sensor data here.
+
+    # Enter here functions to send actuator commands, like:
+    #  motor.setPosition(10.0)
+    pass
+
+# Enter here exit cleanup code.
